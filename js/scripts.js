df0ae7c1a4e5274acc378596c8bdb4f6d37eb751
Patryk Uran <trykpa@gmail.com>
2017-04-21 17:16:11 +0200

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt("Podstawa trójkąta wynosi:");
+var h = prompt("Wysokość trójkąta wynosi:");
+var triangleArea = a*h/2;
+
+console.log("Pole trójkąta o podstawie a: " + a + " i wysokości h: " + h + " wynosi: " + triangleArea);
